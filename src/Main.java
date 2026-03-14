218c7d01d7e3009a14e528272584dc5312e3c241
Bishop Daheu <bishopdaheu123@gmail.com>
2026-03-14 13:09:32 -0400

    Completed Program

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public static void main(String[] args){
+/*
+Bishop Daheu
+Lab - 03
+03/14/2026
+Recursion in Java
+ */
 
-}
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    // Recursive method to compute the series
+    public static double m(int i) {
+
+        if (i == 1) {           // Base case
+            return 1.0;
+        }
+
+        return (1.0 / i) + m(i - 1);   // Recursive case
     }
+
+    // Method to display the series
+    public static void displaySeries() {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("m(%d) = %.4f\n", i, m(i));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        displaySeries();
+    }
+
 }
